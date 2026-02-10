d27c7c6b3f3a8813391fa5fd0e4e47bb6b1103de
Hector Acosta <1Hector.AC@gmail.com>
2026-02-10 14:22:12 -0800

    Small text edit in MoveTheaterDbContext

--- MovieTheaterApplication/Data/MovieTheaterDbContext.cs
+++ MovieTheaterApplication/Data/MovieTheaterDbContext.cs
@@ -11,6 +11,11 @@ namespace MovieTheaterApplication.Data
         {
             base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<Ticket>().HasOne(t => t.Seat).WithMany(s => s.Tickets).HasForeignKey(t => t.SeatId).OnDelete(DeleteBehavior.Restrict); 
+
+            modelBuilder.Entity<Ticket>().HasOne(t => t.Showing).WithMany(sh => sh.Tickets).HasForeignKey(t => t.ShowingId).OnDelete(DeleteBehavior.Restrict);
+
+
         }
 
         public DbSet<Auditorium> Auditoriums { get; set; }
@@ -21,7 +26,7 @@ namespace MovieTheaterApplication.Data
 
         public DbSet<Showing> Showings { get; set; }
 
-        public DbSet<Ticket> Ticket { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
 
     }
 
