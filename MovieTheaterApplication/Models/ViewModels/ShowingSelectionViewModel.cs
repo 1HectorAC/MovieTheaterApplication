b0339997b6149e4a9977ff3a044471842d51da16
Hector Acosta <1Hector.AC@gmail.com>
2026-02-18 12:03:29 -0800

    Added ShowingSelectionViewModel

---
+++ MovieTheaterApplication/Models/ViewModels/ShowingSelectionViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieTheaterApplication.Models.ViewModels
+{
+    public class ShowingSelectionViewModel
+    {
+        public int Id { get; set; }
+
+        public DateTime ShowingTime { get; set; }
+
+        public required string AuditoriumName { get; set;  }
+    }
+}
