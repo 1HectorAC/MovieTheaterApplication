8e379dc87d1cd8b6d47d70d351e8b895f61e405d
Hector Acosta <1Hector.AC@gmail.com>
2026-02-24 14:47:52 -0800

    Added SeatListViewModel

---
+++ MovieTheaterApplication/Models/ViewModels/SeatListViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieTheaterApplication.Models.ViewModels
+{
+    public class SeatListViewModel
+    {
+        public int SeatId { get; set; }
+
+        public char Row { get; set; }
+
+        public int Column { get; set; }
+
+        public bool IsSelected { get; set; }
+
+    }
+}
