91dbc6d17f18562b1e9b7e8f1b445419012e9edd
Hector Acosta <1Hector.AC@gmail.com>
2026-03-07 15:34:14 -0800

    Added ShowingViewModel

---
+++ MovieTheaterApplication/Models/ViewModels/ShowingViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieTheaterApplication.Models.ViewModels
+{
+    public class ShowingViewModel
+    {
+        public int Id { get; set; }
+
+        public DateTime ShowingTime { get; set; }
+
+    }
+}
